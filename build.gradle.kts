b11493c20754c45e809e80638a2f7f40e3242192
jhdcruz <jhdcrux@outlook.com>
2023-01-06 21:19:59 +0800

    fix missing dependency `dokka`

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.dokka") version "1.7.20"
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
     `java-library`
     `maven-publish`
