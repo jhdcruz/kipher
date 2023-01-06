3b3f41381da611b00e6c4cf91c9d9b4a1735b2af
jhdcruz <jhdcrux@outlook.com>
2023-01-06 12:48:49 +0800

    upgrade to kotlin 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
     `java-library`
     `maven-publish`
