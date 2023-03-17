a0a09f0d98be49c4a5e4e38786c077744e72b6c8
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-03-17 15:19:04 +0800

    aes!: deprecate obsolete methods

--- kipher-aes/src/main/kotlin/kipher/aes/AesModes.kt
+++ kipher-aes/src/main/kotlin/kipher/aes/AesModes.kt
@@ -7,16 +7,4 @@ enum class AesModes(val mode: String) {
 
     /** `AES/CBC/PKCS5Padding` */
     CBC("AES/CBC/PKCS5Padding"),
-
-    /** `AES/CTR/NoPadding` */
-    @Deprecated("Will be phased out, use the default GCM instead.", level = DeprecationLevel.WARNING)
-    CTR("AES/CTR/NoPadding"),
-
-    /** `AES/CFB/NoPadding` */
-    @Deprecated("Will be phased out, use the default GCM instead.", level = DeprecationLevel.WARNING)
-    CFB("AES/CFB/NoPadding"),
-
-    /** `AES/OFB/NoPadding` */
-    @Deprecated("Will be phased out, use the default GCM instead.", level = DeprecationLevel.WARNING)
-    OFB("AES/OFB/NoPadding"),
 }
