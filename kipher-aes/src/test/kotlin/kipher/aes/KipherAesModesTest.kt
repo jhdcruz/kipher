0645b0a7267e7490b7f3034297836f02bc6d63e9
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-03-17 18:27:22 +0800

    aes: remove deprecated method tests

--- kipher-aes/src/test/kotlin/kipher/aes/KipherAesModesTest.kt
+++ kipher-aes/src/test/kotlin/kipher/aes/KipherAesModesTest.kt
@@ -1,8 +1,6 @@
 package kipher.aes
 
-import kipher.common.KipherException
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 internal class KipherAesModesTest {
@@ -17,62 +15,6 @@ internal class KipherAesModesTest {
         val decrypted = kipherAes.decrypt(cipherText, secretKey)
 
         assertEquals(message, String(decrypted, Charsets.UTF_8))
-    }
-
-    @Test
-    fun `test encryption using CTR`() {
-        val kipherAes = KipherAes(256, AesModes.CTR)
-        val secretKey = kipherAes.generateKey()
-
-        val cipherText = kipherAes.encrypt(message, secretKey)
-        val decrypted = kipherAes.decrypt(cipherText, secretKey)
-
-        assertEquals(message, String(decrypted, Charsets.UTF_8))
-    }
-
-    @Test
-    fun `test encryption using CFB`() {
-        val kipherAes = KipherAes(256, AesModes.CFB)
-        val secretKey = kipherAes.generateKey()
-
-        val cipherText = kipherAes.encrypt(message, secretKey)
-        val decrypted = kipherAes.decrypt(cipherText, secretKey)
-
-        assertEquals(message, String(decrypted, Charsets.UTF_8))
-    }
-
-    @Test
-    fun `test encryption using OFB`() {
-        val kipherAes = KipherAes(256, AesModes.OFB)
-        val secretKey = kipherAes.generateKey()
-
-        val cipherText = kipherAes.encrypt(message, secretKey)
-        val decrypted = kipherAes.decrypt(cipherText, secretKey)
-
-        assertEquals(message, String(decrypted, Charsets.UTF_8))
-    }
-
-    @Test
-    fun `test encryption using mode that does not support metadata`() {
-        val kipherAes = KipherAes(256, AesModes.OFB)
-        val secretKey = kipherAes.generateKey()
-
-        assertThrows<KipherException> {
-            kipherAes.encrypt(message, "metadata".encodeToByteArray(), secretKey)
-        }
-    }
-
-    @Test
-    fun `test decryption using mode that does not support metadata`() {
-        val kipherAes = KipherAes(256, AesModes.OFB)
-        val secretKey = kipherAes.generateKey()
-
-        val cipherText = kipherAes.encrypt(message, secretKey)
-        val decrypted = kipherAes.decrypt(cipherText, secretKey)
-
-        assertThrows<KipherException> {
-            kipherAes.decrypt(decrypted, "metadata".encodeToByteArray(), secretKey)
-        }
     }
 
     @Test
