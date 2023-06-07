3f9d404760d83a41db0c1f21a08d8661747ddadc
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-06-07 20:29:37 +0800

    fix(compat): java docs and sources for compat modules

--- kipher-aes-compat/build.gradle.kts
+++ kipher-aes-compat/build.gradle.kts
@@ -12,3 +12,50 @@ dependencies {
         configurations.shadow
     }
 }
+
+tasks {
+    shadowJar {
+        dependsOn(project(":kipher-aes").tasks.shadowJar)
+    }
+
+    dokkaHtml {
+        outputDirectory.set(buildDir.resolve("javadoc"))
+
+        dokkaSourceSets {
+            configureEach {
+                dependsOn(project(":kipher-aes").tasks.dokkaHtml)
+            }
+        }
+    }
+
+    dokkaHtmlPartial.configure {
+        dokkaSourceSets {
+            configureEach {
+                sourceRoots.from(project(":kipher-aes").sourceSets.main.get().allSource.srcDirs)
+            }
+        }
+    }
+
+    dokkaJavadoc {
+        outputDirectory.set(buildDir.resolve("javadoc"))
+
+        dokkaSourceSets {
+            configureEach {
+                sourceRoots.from(project(":kipher-aes").sourceSets.main.get().allSource.srcDirs)
+            }
+        }
+    }
+
+    javadocJar {
+        dependsOn(dokkaJavadoc)
+        archiveClassifier.set("javadoc")
+
+        from(dokkaJavadoc)
+    }
+
+    // configure sources jar
+    sourcesJar {
+        archiveClassifier.set("sources")
+        from(project(":kipher-aes").sourceSets.main.get().allSource)
+    }
+}
--- kipher-rsa-compat/build.gradle.kts
+++ kipher-rsa-compat/build.gradle.kts
@@ -12,3 +12,50 @@ dependencies {
         configurations.shadow
     }
 }
+
+tasks {
+    shadowJar {
+        dependsOn(project(":kipher-rsa").tasks.shadowJar)
+    }
+
+    dokkaHtml {
+        outputDirectory.set(buildDir.resolve("javadoc"))
+
+        dokkaSourceSets {
+            configureEach {
+                dependsOn(project(":kipher-rsa").tasks.dokkaHtml)
+            }
+        }
+    }
+
+    dokkaHtmlPartial.configure {
+        dokkaSourceSets {
+            configureEach {
+                sourceRoots.from(project(":kipher-rsa").sourceSets.main.get().allSource.srcDirs)
+            }
+        }
+    }
+
+    dokkaJavadoc {
+        outputDirectory.set(buildDir.resolve("javadoc"))
+
+        dokkaSourceSets {
+            configureEach {
+                sourceRoots.from(project(":kipher-rsa").sourceSets.main.get().allSource.srcDirs)
+            }
+        }
+    }
+
+    javadocJar {
+        dependsOn(dokkaJavadoc)
+        archiveClassifier.set("javadoc")
+
+        from(dokkaJavadoc)
+    }
+
+    // configure sources jar
+    sourcesJar {
+        archiveClassifier.set("sources")
+        from(project(":kipher-rsa").sourceSets.main.get().allSource)
+    }
+}
