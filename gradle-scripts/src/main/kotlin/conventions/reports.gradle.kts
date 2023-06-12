caa3f98661a13d7e2469bb4ea733f710ad1294f7
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-06-12 21:22:47 +0800

    gradle: add separate plugins for reporting

--- gradle-scripts/src/main/kotlin/conventions/reports.gradle.kts
+++ gradle-scripts/src/main/kotlin/conventions/reports.gradle.kts
@@ -6,30 +6,47 @@ import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 plugins {
     id("io.gitlab.arturbosch.detekt")
     id("org.sonarqube")
+    jacoco
+    `java-library`
+}
+
+@Suppress("UnstableApiUsage")
+reporting {
+    reports {
+        creating(JacocoCoverageReport::class) {
+            testType.set(TestSuiteType.UNIT_TEST)
+        }
+    }
 }
 
 sonarqube {
     properties {
         val projectKey = "${rootProject.property("GROUP")}:${project.property("POM_NAME")}"
 
+        // project metadata
         property("sonar.projectKey", projectKey)
         property("sonar.organization", rootProject.property("sonar.organization").toString())
         property("sonar.host.url", rootProject.property("sonar.host.url").toString())
         property("sonar.projectDescription", project.property("POM_DESCRIPTION").toString())
         property("sonar.projectVersion", project.property("VERSION_NAME").toString())
 
+        // project reports
         property(
             "sonar.coverage.jacoco.xmlReportPaths",
-            "${rootProject.projectDir}/kipher-coverage/build/reports/jacoco/testCodeCoverageReport/testCodeCoverageReport.xml"
+            "${project.projectDir}/build/reports/jacoco/test/jacocoTestReport.xml"
+        )
+        property(
+            "sonar.kotlin.detekt.reportPaths",
+            "${project.projectDir}/build/reports/detekt/detekt.xml"
         )
     }
 }
 
-val detektReportMergeSarif by tasks.registering(ReportMergeTask::class) {
-    output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif"))
-}
-
 tasks {
+    val detektReportMergeSarif by tasks.registering(ReportMergeTask::class) {
+        output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif"))
+    }
+
     withType<Detekt>().configureEach {
         basePath = rootProject.projectDir.absolutePath
         jvmTarget = "1.8"
@@ -46,5 +63,18 @@ tasks {
     // Merge detekt report into sarif file for CodeQL scanning
     detektReportMergeSarif.configure {
         input.from(withType<Detekt>().map { it.sarifReportFile })
+    }
+
+    named<JacocoReport>("jacocoTestReport") {
+        dependsOn(test)
+
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+    test {
+        finalizedBy(named("jacocoTestReport"))
     }
 }
