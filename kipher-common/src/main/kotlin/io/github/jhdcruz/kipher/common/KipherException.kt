a1505a4763cd59b6a9eb98c1b4efc0b559b033c6
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-06-15 16:43:31 +0800

    refactor(common): inherit `Exception` instead
    + add throwable error in message constructor

--- kipher-common/src/main/kotlin/io/github/jhdcruz/kipher/common/KipherException.kt
+++ kipher-common/src/main/kotlin/io/github/jhdcruz/kipher/common/KipherException.kt
@@ -8,9 +8,9 @@ package io.github.jhdcruz.kipher.common
 /**
  * Throw custom exception caused by [cause].
  *
- * @constructor creates a new [RuntimeException] with the entire exception
+ * @constructor creates a new [Exception] with the entire exception
  */
-class KipherException : RuntimeException {
+class KipherException : Exception {
     constructor(e: Throwable?) : super(e)
-    constructor(message: String) : super(message)
+    constructor(message: String, e: Throwable?) : super(message, e)
 }
