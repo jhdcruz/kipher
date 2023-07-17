32e6393e0854d3a4fa9184fcb4e6c5fee72203e4
Joshua Hero <jhdcrux@outlook.com>
2023-07-17 16:00:39 +0800

    fix(coverage): add digest and mac on aggregated coverage
    Signed-off-by: Joshua Hero <jhdcrux@outlook.com>

--- kipher-coverage/build.gradle.kts
+++ kipher-coverage/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 dependencies {
     jacocoAggregation(projects.kipherCommon)
     jacocoAggregation(projects.kipherAes)
+    jacocoAggregation(projects.kipherDigest)
+    jacocoAggregation(projects.kipherMac)
 }
 
 tasks {
